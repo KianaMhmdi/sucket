9f8807631133e505dfc7788fbc2042af6fcb71b9
Kiana <kiana.mohamadi2004@gmail.com>
2025-05-17 23:21:25 +0330

    Update clientF1.py

--- faz1/clientF1.py
+++ faz1/clientF1.py
@@ -1,12 +1,16 @@
-import socket
+import socket  # ایمپورت کردن ماژول socket برای ارتباطات شبکه
 
-HOST = '127.0.0.1'  
-PORT = 5000       
+HOST = '127.0.0.1'  # تعریف آدرس IP سروری که می خواهیم به آن متصل شویم (در اینجا localhost)
+PORT = 5000  # تعریف شماره پورت سروری که می خواهیم به آن متصل شویم
 
+# ایجاد یک شیء socket با استفاده از خانواده آدرس IPv4 و پروتکل TCP
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    message = "hello server!"
-    s.sendall(message.encode('utf-8'))
-    data = s.recv(1024)
-    print(f"received: {data.decode('utf-8')}")
-    print("The connection was closed.")
+    s.connect((HOST, PORT))  # اتصال به سرور در آدرس و پورت مشخص شده
+
+    message = "hello server!"  # تعریف پیامی که می خواهیم به سرور ارسال کنیم
+    s.sendall(message.encode('utf-8'))  # کدگذاری پیام به بایت با استفاده از UTF-8 و ارسال آن به سرور
+
+    data = s.recv(1024)  # دریافت حداکثر 1024 بایت داده از سرور
+    print(f"received: {data.decode('utf-8')}")  # رمزگشایی داده های دریافتی از UTF-8 و چاپ آن
+
+    print("The connection was closed.")  # چاپ پیامی مبنی بر اینکه اتصال بسته شده است
